15b2b2846c7ffe3f938a117979c69748546d4621
Samuel Williams <williamssamuel1999@gmail.com>
2018-11-23 21:16:05 +0000

    Initial fix

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 5000;
 
