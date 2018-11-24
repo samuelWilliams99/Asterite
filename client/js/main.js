3ebe7828b7dd6b40b4027092eef97cee71bed8b0
Joshua Richardson <psyjr4@nottingham.ac.uk>
2018-11-24 19:06:42 +0000

    Added leaderboard visuals, seems to work but lost train of thought a few times while rearranging css so might be some wierd stuff

--- client/js/main.js
+++ client/js/main.js
@@ -6,3 +6,13 @@ function joinGame(e){
     console.log(name);
     socket.emit('playerJoin', name);
 }
+
+function toggleLeaderBoard(){
+    if($('#leaderboard').hasClass('hidden')){
+        $('#leaderboard').removeClass('hidden');
+        document.getElementById("toggle-leaderboard").innerHTML = ">";
+    }else{
+        $('#leaderboard').addClass('hidden');
+        document.getElementById("toggle-leaderboard").innerHTML = "<";
+    }
+}
