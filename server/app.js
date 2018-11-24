f094ec43eea38cd215e8ce2920bf6af260c1253c
Joshua Richardson <psyjr4@nottingham.ac.uk>
2018-11-24 15:00:15 +0000

    Created adata structure for players in a new file called players.js and then instatiate in app.js

--- server/app.js
+++ server/app.js
@@ -4,12 +4,16 @@ module.exports = {
 const p2 = require("p2");
 const World = require("./world.js");
 
+const Player = require('./player');
+
 var gameWorld = new World();
 
 function server(io){
 	io.on('connection', function(socket){
 		socket.on('playerJoin', function(name){
 			console.log("New player name \n>", name);
+			Player.createPlayer(name);
+			console.log(Player.players[name].color);
 		});
 	});
 
