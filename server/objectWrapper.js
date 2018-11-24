5633a7a2f6834f7594bd513c7db81eff8f124183
samuelWilliams99 <sammythew@users.noreply.github.com>
2018-11-24 15:40:42 +0000

    Added ObjectWrapper

---
+++ server/objectWrapper.js
@@ -0,0 +1,15 @@
+module.exports = {
+	ObjectWrapper
+}
+
+const Asteroid = require("./asteroid");
+
+function ObjectWrapper(gameWorld, io){
+	this.gameWorld = gameWorld;
+	this.io = io;
+}
+
+ObjectWrapper.prototype.createAsteroid = function(){
+	var ast = new Asteroid(this.gameWorld.world);
+	this.io.emit("asteroidCreate", ast.sendObj())
+}
