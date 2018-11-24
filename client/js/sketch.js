e064f34e41db53e5f8442a2a7a888d1ccd8b13f1
Inigo Taylor <inigotaylor573@gmail.com>
2018-11-24 20:28:14 +0000

    Add radar

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -2,6 +2,7 @@ var fr = 30;
 var maxSubSteps;
 var fixedTimeStep;
 var lastTimeSeconds;
+var radarSize = 100;
 
 function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
@@ -21,11 +22,23 @@ function draw(){
     background(0);
     fill(0);
     stroke(255);
-
+    
     var timeSeconds = millis();
     lastTimeSeconds = lastTimeSeconds || timeSeconds;
     var timeSinceLastCall = timeSeconds - lastTimeSeconds;
     gameWorld.step(fixedTimeStep, timeSinceLastCall, maxSubSteps);
-
+    
     renderBodies();
+    
+    ellipse(80, windowHeight-80, radarSize, radarSize);
+    stroke(100, 100, 100);
+    var radarCircleSize = 100;
+    for (var i = 0; i < 3; i++){
+        ellipse(80, windowHeight-80, radarCircleSize, radarCircleSize);
+        radarCircleSize -= 20;
+    }
+
+    renderRadar(radarSize);
 }
+
+
