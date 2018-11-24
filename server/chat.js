cc3e4fd9b1f318961622cd28bd9b7a9f007b1951
psytb5 <psytb5@nottingham.ac.uk>
2018-11-24 23:28:41 +0000

    Added random color

--- server/chat.js
+++ server/chat.js
@@ -1,3 +1,8 @@
+/**
+ * Broachasts a message to clients (not sender)
+ * @param {object} socket connection socket
+ * @param {object} payload {from, message, color}
+ */
 function sendMessage(socket, payload) {
     socket.broadcast.emit('sendMessage', payload);
 }
