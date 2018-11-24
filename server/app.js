ff29eceb2fae43a9434da8352cf75417c5ccf3ac
psytb5 <psytb5@nottingham.ac.uk>
2018-11-24 18:46:08 +0000

    Started leaderboard

--- server/app.js
+++ server/app.js
@@ -1,32 +1,28 @@
 module.exports = {
-	serve: server
-}
-const p2 = require("p2");
-const World = require("./world");
+    serve: server
+};
+const p2 = require('p2');
+const World = require('./world');
 
-const ObjectWrapper = require("./objectWrapper");
-
+const ObjectWrapper = require('./objectWrapper');
 const Player = require('./player');
+const Leaderboard = require('./leaderboard');
 
+function server(io) {
+    var gameWorld = new World();
+    var objectWrapper = new ObjectWrapper(gameWorld, io);
 
+    setInterval(function() {
+        objectWrapper.createAsteroid();
+    }, 1000);
 
-function server(io){
-	var gameWorld = new World();
-	var objectWrapper = new ObjectWrapper(gameWorld, io);
-	
-	setInterval(function(){
-		objectWrapper.createAsteroid();
-	}, 1000);
+    io.on('connection', function(socket) {
+        socket.on('playerJoin', function(name) {
+            console.log('New player name \n>', name);
+            var ply = new Player(name);
+            console.log(ply.color);
+        });
 
-	io.on('connection', function(socket){
-		
-		socket.on('playerJoin', function(name){
-			console.log("New player name \n>", name);
-			var ply = new Player(name);
-			console.log(ply.color);
-		});
-	});
-
+        Leaderboard(io, socket);
+    });
 }
-
-
--- server/player.js
+++ server/player.js
@@ -1,15 +1,22 @@
 global.players = {};
+const Leaderboard = require('./leaderboard');
 
-
-function Player(name){
-    
+function Player(name) {
     this.body = '';
     this.shape = '';
+    this.score = 0;
+    this.viewPos = [0, 0];
     this.powerups = '';
     this.color = 'red';
     this.name = name;
 
     players[name] = this;
 }
+
+Player.prototype.setScore = function(score) {
+    this.score = score;
+    const topTen = Leaderboard.getTopTen();
+    Leaderboard.update(topTen);
+};
 
 module.exports = Player;
