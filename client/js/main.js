bce932c931ccd794ccfeb33132b19393eed4ae2f
psytb5 <psytb5@nottingham.ac.uk>
2018-11-25 01:39:33 +0000

    Added duplicate name validation

--- client/js/main.js
+++ client/js/main.js
@@ -1,60 +1,90 @@
-socket.on('leaderboardUpdate', function(scores){
+socket.on('leaderboardUpdate', function(scores) {
     updateLeaderboard(scores);
 });
 
-$(function(){
-    updateLeaderboard([{name: "Tim", color:"red", score: "5000"}, {name: "Inigo", color:"blue", score: "500"}, {name: "Sam", color:"green", score: "50"}, {name: "Joshua", color:"pink", score: "5"}]);
-    updateKillfeed({killer:{name: "Tim", color:"red"}, killed:{name: "Joshua", color:"pink"}, weapon: "Skittles"});
+socket.on('joinError', function(errorMessage) {
+    $('#join-error').text(errorMessage);
+    $('#join-error').removeClass('hidden');
 });
 
-function joinGame(e){
+socket.on('joinSuccess', function() {
+    $('#player-name-dialog, #join-error').addClass('hidden');
+});
+
+$(function() {
+    // updateLeaderboard([
+    //     { name: 'Tim', color: 'red', score: '5000' },
+    //     { name: 'Inigo', color: 'blue', score: '500' },
+    //     { name: 'Sam', color: 'green', score: '50' },
+    //     { name: 'Joshua', color: 'pink', score: '5' }
+    // ]);
+    // updateKillfeed({
+    //     killer: { name: 'Tim', color: 'red' },
+    //     killed: { name: 'Joshua', color: 'pink' },
+    //     weapon: 'Skittles'
+    // });
+});
+
+function joinGame(e) {
     e.preventDefault();
     var name = document.getElementById('userName').value;
-    $('#player-name-dialog').addClass('hidden');
-    console.log(name);
     socket.emit('playerJoin', name);
 }
 
-function togglePane(e){
-    var id = "#" + e.currentTarget.id;
-    var targetViewId = "#" + $(id).data("target-view");
-    if($(targetViewId).hasClass('hidden')){
+function togglePane(e) {
+    var id = '#' + e.currentTarget.id;
+    var targetViewId = '#' + $(id).data('target-view');
+    if ($(targetViewId).hasClass('hidden')) {
         $(targetViewId).removeClass('hidden');
-    }else{
+    } else {
         $(targetViewId).addClass('hidden');
     }
 }
 
-
-function updateLeaderboard(scores){
-    var tableString = "";
-    for(var i = 0; i < scores.length; i++){
-        if(i==0){
-            myColor="#e09e1a";
-        }else if(i==1){
-            myColor="#719eb7";
-        }else if(i==2){
-            myColor = "#ad6114";
-        }else{
-            myColor = "white";
+function updateLeaderboard(scores) {
+    var tableString = '';
+    for (var i = 0; i < scores.length; i++) {
+        if (i == 0) {
+            myColor = '#e09e1a';
+        } else if (i == 1) {
+            myColor = '#719eb7';
+        } else if (i == 2) {
+            myColor = '#ad6114';
+        } else {
+            myColor = 'white';
         }
-        tableString += "<tr>";
-        tableString += "<td class=\"pos\" style=\"color:"+ myColor +"\">" + (i+1)+"." + "</td>";
-        tableString += "<td class=\"name\" style=\"color:"+ scores[i].color +"\">" + scores[i].name + "</td>";
-        tableString += "<td class=\"score\" style=\"color:"+ myColor +"\">" + scores[i].score + "</td>";
-        tableString += "</tr>";
-        
+        tableString += '<tr>';
+        tableString +=
+            '<td class="pos" style="color:' +
+            myColor +
+            '">' +
+            (i + 1) +
+            '.' +
+            '</td>';
+        tableString +=
+            '<td class="name" style="color:' +
+            scores[i].color +
+            '">' +
+            scores[i].name +
+            '</td>';
+        tableString +=
+            '<td class="score" style="color:' +
+            myColor +
+            '">' +
+            scores[i].score +
+            '</td>';
+        tableString += '</tr>';
     }
 
-    document.getElementById("leaderboard__table").innerHTML = tableString;						
+    document.getElementById('leaderboard__table').innerHTML = tableString;
 }
 
-function updateKillfeed(killObj){
-    document.getElementById("killfeed__text").innerHTML = "<span id=\"killer__span\"></span> -> <span id=\"killed__span\"></span> with <span id=\"weapon__span\"></span>";
-    document.getElementById("killer__span").innerHTML = killObj.killer.name;
-    document.getElementById("killer__span").style.color = killObj.killer.color;
-    document.getElementById("killed__span").innerHTML = killObj.killed.name;
-    document.getElementById("killed__span").style.color = killObj.killed.color;
-    document.getElementById("weapon__span").innerHTML = killObj.weapon;
-
+function updateKillfeed(killObj) {
+    document.getElementById('killfeed__text').innerHTML =
+        '<span id="killer__span"></span> -> <span id="killed__span"></span> with <span id="weapon__span"></span>';
+    document.getElementById('killer__span').innerHTML = killObj.killer.name;
+    document.getElementById('killer__span').style.color = killObj.killer.color;
+    document.getElementById('killed__span').innerHTML = killObj.killed.name;
+    document.getElementById('killed__span').style.color = killObj.killed.color;
+    document.getElementById('weapon__span').innerHTML = killObj.weapon;
 }
--- server/app.js
+++ server/app.js
@@ -34,10 +34,15 @@ function server() {
         var viewPos = [gameWorld.worldSize / 2, gameWorld.worldSize / 2];
         socket.viewPos = viewPos;
         socket.on('playerJoin', function(name) {
-            console.log('New player name \n>', name);
-            var ply = new Player(socket, name, gameWorld);
-            ply.setScore(0);
-            console.log(ply.color);
+            if (typeof players[name] !== 'undefined') {
+                socket.emit('joinError', 'This name is taken!');
+            } else {
+                console.log(name + ' joined the game!');
+                var ply = new Player(socket, name, gameWorld);
+                ply.setScore(0);
+                socket.emit('joinSuccess');
+                Leaderboard.update();
+            }
         });
 
         socket.on('asteroidRequestData', function(ids) {
