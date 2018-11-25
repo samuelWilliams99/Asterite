d4af5e149d63d642e662b75f7830c45167763396
samuelWilliams99 <sammythew@users.noreply.github.com>
2018-11-25 11:11:24 +0000

    Finished dying

--- client/js/game.js
+++ client/js/game.js
@@ -35,7 +35,9 @@ function createPlayer(playerData){
 }
 
 function removePlayer(name){
+    
 	if(Players[name]){
+		openStartDialog(Players[name].score, name);
 		gameWorld.removeBody(Players[name].body);
 		delete Players[name];
 		if(username == name){
