b3768dd95f4d3daea4a3cb1ede5547b42f3867d2
Joshua Richardson <psyjr4@nottingham.ac.uk>
2018-11-25 09:27:30 +0000

    Finished chat for now

--- server/chat.js
+++ server/chat.js
@@ -8,8 +8,11 @@ var messages = [];
 
 function sendMessage(socket, payload) {
     // socket.broadcast.emit('sendMessage', payload);
-    console.log(2);
-    var data = {name:socket.name, color:players[socket.name].color, message:payload }
+    if(socket.name == null){
+        var data = {name:"Guest", color:"orange", message:payload }
+    }else{
+        var data = {name:socket.name, color:players[socket.name].color, message:payload }
+    }
     messages.push(data);
     var n = messages.slice(Math.max(0, messages.length-5), messages.length);
     io.emit('chatUpdate', n);
