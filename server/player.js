11a355dc3c27a816798b1783b5af8e308014614e
samuelWilliams99 <sammythew@users.noreply.github.com>
2018-11-25 00:02:15 +0000

    Player started

--- server/player.js
+++ server/player.js
@@ -1,11 +1,28 @@
 global.players = {};
 const Leaderboard = require('./leaderboard');
 
-function Player(name, socket) {
+var plyShape = [[0, -6], [-3, 2],[3, 2]];
+
+function Player(socket, name, World) {
     this.socket = socket;
+    this.World = World;
+    this.world = World.world;
     socket.name = name;
-    this.body = '';
-    this.shape = '';
+    this.body = new p2.Body({
+        mass: 10,
+        position: [this.World.worldSize/2,this.World.worldSize/2],
+        //position: [getRandomInt(100,1800), 500],
+        angle: 0,
+        velocity: [0, 0],
+        angularVelocity: 0,
+        damping: 0,
+        angularDamping: 0,
+        inertia: 150
+    });
+    this.shape = new p2.Convex({vertices: plyShape});
+    this.body.addShape(this.shape);
+    this.world.addBody(this.body);
+    
     this.score = 0;
     this.position = [15000, 15000];
     this.powerups = '';
