f4fe685ebd57cc23f54733bb2576137ca6abdad9
samuelWilliams99 <sammythew@users.noreply.github.com>
2018-11-25 08:32:13 +0000

    Playing dying may happen now

--- server/world.js
+++ server/world.js
@@ -7,9 +7,44 @@ function World(){
         applyGravity: false,
         frictionGravity: 0
     });
+    this.world.on("beginContact", function(data){
+        bodyA = data.bodyA;
+        bodyB = data.bodyB;
+        var plyA = getPlayerFromBody(bodyA);
+        var plyB = getPlayerFromBody(bodyB);
+        if(plyA && plyA.objectType == "Player") {
+            if(!plyB || plyB.objectType == "Asteroid") {
+                plyA.kill({killer: {name: "themself", color: "#FFFFFF"}, weapon:"pain"});
+            }
+            
+        }
+        if(plyB && plyB.objectType == "Player") {
+            if(!plyA || plyA.objectType == "Asteroid") {
+                plyB.kill({killer: {name: "themself", color: "#FFFFFF"}, weapon:"pain"});
+            }
+        }
+    })
     this.worldSize = 30000;
 	this.createMaterials(new p2.Material());
 };
+
+function getPlayerFromBody(body){
+    if(!body){return null;}
+    var ret = null;
+    for(var key in players){
+        var ply = players[key];
+        if(ply.body.id == body.id){
+            ret = ply;
+        }
+    };
+    for(var key in Asteroids){
+        var ast = Asteroids[key];
+        if(ast.body.id == body.id){
+            ret = ast;
+        }
+    };
+    return ret;
+}
 
 World.prototype.createMaterials = function(material) {
     this.world.addContactMaterial(new p2.ContactMaterial(material, material, {
